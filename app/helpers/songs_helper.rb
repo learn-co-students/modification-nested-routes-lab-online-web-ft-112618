f7f1f991775727d29e053292859c4add70caec23
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-18 11:30:53 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def artist_id_field(form_builder)
+    if form_builder.object.artist
+      form_builder.hidden_field :artist_id
+    else
+      form_builder.collection_select :artist_id, Artist.all, :id, :name
+    end
+  end
+
 end
