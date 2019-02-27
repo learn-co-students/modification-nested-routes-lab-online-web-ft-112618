980fca9c8bd89380426b5af21e71bbca61b6aa97
ghankeliu2015 <hanke.liu@gmail.com>
2019-02-27 12:48:04 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,15 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist_name, song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      # hidden_field_tag "song[artist_id]", song.artist_id
+      #display_artist(song)
+      song.artist_name
+    end
+
+  end
 end
